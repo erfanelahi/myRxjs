a63ee6b9ea74ffba82a23a1b5c619605bd2839c3
erfanelahi <erfan.elahi@gmail.com>
2016-12-20 01:02:25 +0600

    more update

--- practiceRxjs.js
+++ practiceRxjs.js
@@ -1,5 +1,4 @@
 //var Rx = require('rx');
-
 var arr = [1, 2, 3, "hi", 7, 8, 9, "world"];
 // off
 var objOf = Rx.Observable.of(...arr).take(3).map(x => parseInt(x))
@@ -15,11 +14,23 @@ objMerge.subscribe(x => console.log(x));
 // just / flatMap / fromPromise
 var objJustFlatMap = Rx.Observable.just("https://api.github.com/users")
     .flatMap(requestURL => Rx.Observable.fromPromise(jQuery.getJSON(requestURL)))
-    .subscribe(x => console.log(x));
-// buffer / throttle
+// buffer / delay
 var countClick = document.querySelector("#countClick");
 var clickResult = document.querySelector("#clickResult");
 var clickStream = Rx.Observable.fromEvent(countClick, "click");
-var dbclickStream = clickStream.buffer(() => clickStream.throttle(250)).map(arr => arr.length)//.filter(len => len === 2);
-dbclickStream.subscribe(a => clickResult.textContent = "Double Clicked!!!")
-dbclickStream.throttle(1000).subscribe(a => clickResult.textContent = "---")
+var evenTimeStampStream = clickStream.buffer(() => clickStream.delay(500))
+    .map(event => { console.log("Click Count : "+event.length); return event.length; }).filter(length => length === 2);
+evenTimeStampStream.subscribe(event => {
+    clickResult.innerHTML = "";
+    objJustFlatMap.subscribe(data =>
+        data.forEach(item =>
+            clickResult.innerHTML += `<div style='border: 1px solid #e4e6e8;'>
+            <img style="float:laft" src="${item.avatar_url}" alt="avatar" width="100" height="100" />
+            <a style="float:laft" target="_blank" href="${item.html_url}">${item.login}</a>
+        </div>`))
+})
+var timer1 = Rx.Observable.interval(2000).take(2);
+var timer2 = Rx.Observable.interval(1000).take(3);
+var timer3 = Rx.Observable.interval(500).take(4);
+var result = timer1.concat(timer2, timer3);
+result.subscribe(x => console.log(x));
