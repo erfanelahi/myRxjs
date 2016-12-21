deab584272dd72ef30c5b4711c3aad74e3492e07
erfanelahi <erfan.elahi@gmail.com>
2016-12-22 02:27:17 +0600

    last up

--- practiceRxjs.js
+++ practiceRxjs.js
@@ -33,7 +33,7 @@ var timer1 = Rx.Observable.interval(2000).take(2);
 var timer2 = Rx.Observable.interval(1000).take(3);
 var timer3 = Rx.Observable.interval(500).take(4);
 var timer4 = Rx.Observable.interval(1000);
-var result = timer2.concat(timer3, timer1).withLatestFrom(timer4);
+var result = timer2.concat(timer3, timer1).withLatestFrom(timer4, (t123, t4) => [t123, t4 ]);
 result.subscribe(x => console.log(x));
 // range / combineLatest 
 var weight = Rx.Observable.of(70, 72, 76, 79, 75);
