f28a4442826d57bb02f1358b9094821c505d87e2
eelahi <eelahi@ael-bd.com>
2016-12-27 13:15:12 +0600

    Begin

--- practiceRxjs.js
+++ practiceRxjs.js
@@ -168,7 +168,7 @@ checked.filter(x => x === true).flatMapLatest(() => dotSource.takeUntil(checked)
 // 
 var recordedVideo = Rx.Observable.interval(1000).take(5);
 recordedVideo.subscribe(x => console.log("%c Recorded 1 :" + x, 'color: black'));
-setTimeout(() => recordedVideo.subscribe(x => console.log("%c Recorded 2 :" + x, 'color: black')), 3000);
+setTimeout(() => recordedVideo.subscribe(x => console.log("%c Recorded 2 :" + x, 'color: orange')), 3000);
 // publish / refCount / *connect
 var liveVideo = Rx.Observable.interval(1000).take(5).publish().refCount();
 liveVideo.subscribe(x => console.log("%c Live 1 :" + x, 'color: green'));
