a70f60651d42fbe26a46ab005a5d5110780595e3
erfanelahi <erfan.elahi@gmail.com>
2017-01-20 23:34:27 +0600

    Lazy Store

--- src/app/lazy/lazy.component.ts
+++ src/app/lazy/lazy.component.ts
@@ -1,17 +1,37 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Store } from '@ngrx/store';
+import { HOMEVALUE, AppState } from '../reducer';
 
 @Component({
   selector: 'my-lazy',
   templateUrl: './lazy.component.html'
+  , changeDetection: ChangeDetectionStrategy.OnPush
 })
-
-export class LazyComponent {
+export class LazyComponent implements OnDestroy {
   @ViewChild('formRef') formRef;
   ngAfterViewInit() {
-    this.formRef.valueChanges.subscribe(v => console.table(v));
+    //this.formRef.valueChanges.subscribe(v => console.table(v));
   }
   onSubmit(formValues) {
     console.table(formValues);
   }
   locations: string[] = ["Home", "Away"];
+  //------------------------------------
+  homeValueLazy: Observable<string>;
+  currentValueSubscriptionLazy: Subscription;
+  constructor(private storeLazy: Store<AppState>) {
+    this.homeValueLazy = this.storeLazy.select<string>('data');
+  }
+  setHomeValueLazy(value) {
+    let currentValueLazy;
+    this.currentValueSubscriptionLazy = this.storeLazy.select<string>('data').subscribe(value => currentValueLazy = value);
+    if (currentValueLazy !== value) {
+      this.storeLazy.dispatch({ type: HOMEVALUE, payload: value });
+    }
+  }
+  ngOnDestroy() {
+    this.currentValueSubscriptionLazy && this.currentValueSubscriptionLazy.unsubscribe();
+  }
 }
